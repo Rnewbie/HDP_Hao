43d76db5d6678d92a0abb2d2808a0fee0f158971
Saw Simeon <doe_doe207@hotmail.com>
2015-09-08 17:36:39 +0700

    ok

--- HDP.R
+++ HDP.R
@@ -337,6 +337,216 @@ pca <- prcomp(df, retx=TRUE,scale.=TRUE)
 summary(pca)
 
 
+### Correlation Descriptors
+extractMoran = function (x, props = c('CIDH920105', 'BHAR880101',
+                                      'CHAM820101', 'CHAM820102',
+                                      'CHOC760101', 'BIGC670101',
+                                      'CHAM810101', 'DAYM780201'), 
+                         nlag = 30L, customprops = NULL) {
+  
+  if (protcheck(x) == FALSE) stop('x has unrecognized amino acid type')
+  
+  if (nchar(x) <= nlag){
+    warning("extractMoran: The length of the sequence is <= nlag; NA's will result.")
+  }
+  
+  
+  # 1. Compute Pr values for each type of property
+  
+  AAidx = read.csv(system.file('sysdata/AAidx.csv', package = 'protr'), header = TRUE)
+  
+  if (!is.null(customprops)) AAidx = rbind(AAidx, customprops)
+  
+  aaidx = AAidx[, -1]
+  row.names(aaidx) = AAidx[, 1]
+  n = length(props)
+  pmean = rowMeans(aaidx[props, ])
+  psd   = apply(aaidx[props, ], 1, sd) * sqrt((20 - 1)/20) # sd() uses (n-1)
+  
+  Pr = data.frame(matrix(ncol = 20, nrow = n))
+  for (i in 1:n) Pr[i, ] = (aaidx[props[i], ] - pmean[i])/psd[i]
+  
+  # 2. Replace character with numbers, also applies to less than 20 AA occured
+  
+  xSplitted = strsplit(x, split = '')[[1]]
+  AADict = c('A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 
+             'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V')
+  names(Pr) = AADict
+  
+  P = vector('list', n)
+  for (i in 1:n) P[[i]] = xSplitted
+  
+  for (i in 1:n) {
+    for (j in AADict) {
+      try(P[[i]][which(P[[i]] == j)] <- Pr[i, j], silent = TRUE)
+    }
+  }
+  
+  P = lapply(P, as.numeric)
+  
+  # 3. Compute Moran Autocorrelation Descriptor
+  
+  Moran = vector('list', n)
+  N  = length(xSplitted)
+  
+  Pbar = sapply(P, mean)
+  
+  for (i in 1:n) {
+    for (j in 1:nlag) {
+      Moran[[i]][j] = ifelse(N-j > 0,
+                             (N/(N - j)) * ((sum((P[[i]][1:(N - j)] - Pbar[i]) * (P[[i]][(1:(N - j)) + j] - Pbar[i])))/(sum((P[[i]] - Pbar[i])^2))),
+                             NA)
+    }
+  }
+  
+  Moran = unlist(Moran)
+  
+  names(Moran) = as.vector(t(outer(props, 
+                                   paste('.lag', 1:nlag, sep = ''), 
+                                   paste, sep = '')))
+  
+  return(Moran)
+  
+}
+
+
+extractMoreauBroto = function (x, props = c('CIDH920105', 'BHAR880101',
+                                            'CHAM820101', 'CHAM820102',
+                                            'CHOC760101', 'BIGC670101',
+                                            'CHAM810101', 'DAYM780201'), 
+                               nlag = 30L, customprops = NULL) {
+  
+  if (protcheck(x) == FALSE) stop('x has unrecognized amino acid type')
+  
+  if (nchar(x) <= nlag){
+    warning("extractMoreauBroto: The length of the sequence is <= nlag; NA's will result.")
+  }
+  
+  # 1. Compute Pr values for each type of property
+  
+  AAidx = read.csv(system.file('sysdata/AAidx.csv', package = 'protr'), header = TRUE)
+  
+  if (!is.null(customprops)) AAidx = rbind(AAidx, customprops)
+  
+  aaidx = AAidx[, -1]
+  row.names(aaidx) = AAidx[, 1]
+  n = length(props)
+  pmean = rowMeans(aaidx[props, ])
+  psd   = apply(aaidx[props, ], 1, sd) * sqrt((20 - 1)/20) # sd() uses (n-1)
+  
+  Pr = data.frame(matrix(ncol = 20, nrow = n))
+  for (i in 1:n) Pr[i, ] = (aaidx[props[i], ] - pmean[i])/psd[i]
+  
+  # 2. Replace character with numbers, also applies to less than 20 AA occured
+  
+  xSplitted = strsplit(x, split = '')[[1]]
+  AADict = c('A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 
+             'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V')
+  names(Pr) = AADict
+  
+  P = vector('list', n)
+  for (i in 1:n) P[[i]] = xSplitted
+  
+  for (i in 1:n) {
+    for (j in AADict) {
+      try(P[[i]][which(P[[i]] == j)] <- Pr[i, j], silent = TRUE)
+    }
+  }
+  
+  P = lapply(P, as.numeric)
+  
+  # 3. Compute Moreau-Broto Autocorrelation Descriptor
+  
+  MB = vector('list', n)
+  N  = length(xSplitted)
+  
+  for (i in 1:n) {
+    for (j in 1:nlag) {
+      MB[[i]][j] = ifelse(N-j > 0,
+                          sum(P[[i]][1:(N - j)] * P[[i]][(1:(N - j)) + j])/(N - j),
+                          NA)
+    }
+  }
+  
+  MB = unlist(MB)
+  
+  names(MB) = as.vector(t(outer(props, 
+                                paste('.lag', 1:nlag, sep = ''), 
+                                paste, sep = '')))
+  
+  return(MB)
+  
+}
+
+extractGeary = function (x, props = c('CIDH920105', 'BHAR880101',
+                                      'CHAM820101', 'CHAM820102',
+                                      'CHOC760101', 'BIGC670101',
+                                      'CHAM810101', 'DAYM780201'), 
+                         nlag = 30L, customprops = NULL) {
+  
+  if (protcheck(x) == FALSE) stop('x has unrecognized amino acid type')
+  
+  if (nchar(x) <= nlag){
+    warning("extractGeary: The length of the sequence is <= nlag; NA's will result.")
+  }
+  
+  # 1. Compute Pr values for each type of property
+  
+  AAidx = read.csv(system.file('sysdata/AAidx.csv', package = 'protr'), header = TRUE)
+  
+  if (!is.null(customprops)) AAidx = rbind(AAidx, customprops)
+  
+  aaidx = AAidx[, -1]
+  row.names(aaidx) = AAidx[, 1]
+  n = length(props)
+  pmean = rowMeans(aaidx[props, ])
+  psd   = apply(aaidx[props, ], 1, sd) * sqrt((20 - 1)/20) # sd() uses (n-1)
+  
+  Pr = data.frame(matrix(ncol = 20, nrow = n))
+  for (i in 1:n) Pr[i, ] = (aaidx[props[i], ] - pmean[i])/psd[i]
+  
+  # 2. Replace character with numbers, also applies to less than 20 AA occured
+  
+  xSplitted = strsplit(x, split = '')[[1]]
+  AADict = c('A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 
+             'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V')
+  names(Pr) = AADict
+  
+  P = vector('list', n)
+  for (i in 1:n) P[[i]] = xSplitted
+  
+  for (i in 1:n) {
+    for (j in AADict) {
+      try(P[[i]][which(P[[i]] == j)] <- Pr[i, j], silent = TRUE)
+    }
+  }
+  
+  P = lapply(P, as.numeric)
+  
+  # 3. Compute Geary Autocorrelation Descriptor
+  
+  Geary = vector('list', n)
+  N  = length(xSplitted)
+  
+  Pbar = sapply(P, mean)
+  
+  for (i in 1:n) {
+    for (j in 1:nlag) {
+      Geary[[i]][j] = ifelse(N-j > 0,
+                             ((N - 1)/(2 * (N - j))) * ((sum((P[[i]][1:(N - j)] - P[[i]][(1:(N - j)) + j])^2))/(sum((P[[i]] - Pbar[i])^2))),
+                             NA)
+    }
+  }
+  
+  Geary = unlist(Geary)
+  
+  names(Geary) = as.vector(t(outer(props, 
+                                   paste('.lag', 1:nlag, sep = ''), 
+                                   paste, sep = '')))
+  
+  return(Geary)
+  
+}
 
 
 
